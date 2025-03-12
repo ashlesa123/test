3b06efd6d7a41e4265ba8a78371644c53c75bc09
ashlesa123 <ashlesasukla1114@gmail.com>
2025-03-12 20:50:00 +0530

    Update TestApplication.java

--- test/src/main/java/com/test/TestApplication.java
+++ test/src/main/java/com/test/TestApplication.java
@@ -7,6 +7,27 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestApplication {
 
 	public static void main(String[] args) {
+
+		@PostMapping
+    public ResponseEntity<Post> savePost(
+            @RequestBody Post post
+    ) {
+        Post newPost = postService.savePost(post);
+        return new ResponseEntity<>(newPost, HttpStatus.CREATED);
+    }
+
+
+    @GetMapping("/{postId}")
+    public ResponseEntity<?> getPostById(@PathVariable String postId) {
+        Post post = postService.findPostById(postId);
+
+        if (post == null) {
+            return new ResponseEntity<>("Post not found with ID: " + postId, HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(post, HttpStatus.OK);
+    }
+
                  
                 int=100;
 		SpringApplication.run(TestApplication.class, args);
